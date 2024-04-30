f1a4173940cfffc8358236545d03702edf6d0f97
Ahmed Ziabat Ziabat <ahmed.ziabat@protonmail.com>
2024-04-30 23:09:37 +0200

    Dataset grouped by procs_machines

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import numpy as np
 import json
 import glob
 import os
+import re
 
 def init_matrix(P, edges, volume, n_msgs, reward_type="volume", normalize=False):
     adj_matrix = np.zeros((P, P))
     if reward_type == "volume":
         rewards = volume
     elif reward_type == "n_msgs":
+        rewards = n_msgs
+    elif reward_type == "num_msgs":
         rewards = n_msgs
     else:
         raise ValueError("Unsupported reward type")
@@ -18,29 +21,57 @@ def init_matrix(P, edges, volume, n_msgs, reward_type="volume", normalize=False)
 
     if normalize:
         max_cost = np.max(adj_matrix)
-        if max_cost > 0:  # Avoid division by zero
+        if max_cost > 0:
             adj_matrix = adj_matrix / max_cost
 
     return adj_matrix
 
-def process_json_files(folder_path, reward_type="volume", normalize=False):
-    file_paths = glob.glob(os.path.join(folder_path, '*.json')) 
-    matrices = []
+def process_json_file(file_path, reward_type="volume", normalize=False):
+    adj_matrix = None
+    optimal = None
+    with open(file_path, 'r') as f:
+        data = json.load(f)
 
-    for file_path in file_paths:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
+        P = data["Graph"]["P"]
+        edges = data["Graph"]["comms"]["edges"]
+        volume = data["Graph"]["comms"]["volume"]
+        n_msgs = data["Graph"]["comms"]["n_msgs"]
 
-            P = data["Graph"]["P"]
-            edges = data["Graph"]["comms"]["edges"]
-            volume = data["Graph"]["comms"]["volume"]
-            n_msgs = data["Graph"]["comms"]["n_msgs"]
+        adj_matrix = init_matrix(P, edges, volume, n_msgs, reward_type, normalize)
+        optimal = data.get("Benchmark", {}).get("optimal_mapping", np.zeros(shape=(P,)))
 
-            adj_matrix = init_matrix(P, edges, volume, n_msgs, reward_type, normalize)
-            matrices.append(adj_matrix)
+    return adj_matrix, optimal
 
-    stacked_matrices = np.stack(matrices, axis=0, dtype=np.float32)
+
+def by_proc_mach(root_dir=".", reward_type="volume", normalize=False):
     
-    np.savez(f"{folder_path}/all.npz", cost_matrix=stacked_matrices)
+    # Subdir recursive walk
+    for subdir, dirs, files in os.walk(root_dir):
+        # Key is PROCS_MACHINES
+        config_group = {}
+        
+        for file in files:
+            if file.endswith('.json'):
+                # Regex Hell
+                match = re.match(r'.*?(\d+_\d+).*?\.json', file)
+                if match:
+                    key = match.group(1)
+                    if key not in config_group:
+                        config_group[key] = {"matrices": [], "optimals": []}
+                    
+                    file_path = os.path.join(subdir, file)
 
-process_json_files("./8", reward_type="volume", normalize=False)
+                    adj_matrix, optimal = process_json_file(file_path=file_path)
+
+                    config_group[key]["matrices"].append(adj_matrix)
+                    config_group[key]["optimals"].append(optimal)
+
+        for key, value in config_group.items():
+            stacked_matrices = np.stack(value["matrices"], axis=0, dtype=np.float32)
+            print(key, value['optimals'])
+            stacked_optimals = np.stack(value["optimals"], axis=0, dtype=np.int32)
+
+            np.savez(os.path.join(subdir, f"{key}.npz"), cost_matrix=stacked_matrices, optimals=stacked_optimals)
+
+
+by_proc_mach()
